eda6a046f157d79f9dd24de18a9fa05730116290
Will Coster <willcoster@gmail.com>
2017-09-24 16:38:44 -0700

    Add tarsnap and feather

---
+++ install-tools.d/00_tarsnap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+name=tarsnap
+
+check_install() {
+  which tarsnap 2>&1 > /dev/null
+}
+
+install() {
+  wget -O /tmp/tarsnap-deb-packaging-key.asc \
+      https://pkg.tarsnap.com/tarsnap-deb-packaging-key.asc
+  sudo apt-key add /tmp/tarsnap-deb-packaging-key.asc
+  sudo sh -c 'echo "deb http://pkg.tarsnap.com/deb/$(lsb_release -s -c) ./" >> /etc/apt/sources.list.d/tarsnap.list'
+  sudo apt-get update
+  sudo apt-get install -y tarsnap
+}
