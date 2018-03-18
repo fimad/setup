302077b455c679ee229d262e8e3dff1772905438
Will Coster <willcoster@gmail.com>
2018-03-18 15:47:39 -0700

    Add docker-machine

---
+++ install-tools.d/00_docker-machine.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+name=docker-machine
+
+tags=(-work)
+
+check_install() {
+  test -f ~/.local/bin/docker-machine
+}
+
+install() {
+  curl -L \
+    "https://github.com/docker/machine/releases/download/v0.14.0/docker-machine-$(uname -s)-$(uname -m)" \
+    > $HOME/.local/bin/docker-machine
+  chmod +x $HOME/.local/bin/docker-machine
+}
