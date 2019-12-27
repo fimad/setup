36c372e29d7e1af929396de521c16635ea0e0648
Will Coster <willcoster@gmail.com>
2019-12-26 18:33:44 -0800

    Upgrade neovim

--- install-tools.d/00_neovim.sh
+++ install-tools.d/00_neovim.sh
@@ -22,7 +22,7 @@ install() {
   git clone https://github.com/neovim/neovim /tmp/neovim
   (
     cd /tmp/neovim &&
-    git checkout v0.3.1 &&
+    git checkout v0.4.3 &&
     make CMAKE_EXTRA_FLAGS="-DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$HOME/.local/neovim" &&
     make install
   )
